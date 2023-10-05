93b6da0bbadfdaa79a7c68e0e8e9cba508324192
nadya <nadya.vilboa@list.ru>
2023-10-05 16:36:25 +0300

    initial settings

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # eatly-multy-pages - многостраничный сайт ресторана
+
+<p>Порядок установки:</p>
+<ul>
+    <li>npm i</li>
+    <li>npm run start --open</li>
+    <li>Если нужна сборка для production, то npm run build</li>
+</ul>
+<p>Автор: Вильбоа Надежда</p>
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const MiniCssExtractPlugin =
+require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: {
+        filename: path.resolve(__dirname, 'src/index.js')
+    },
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'index.js',
+        assetModuleFilename: '[name][ext]'
+    },
+    devServer: {
+        port: 9000,
+        compress: true,
+        hot: true,
+        static: {
+            directory: path.join(__dirname, 'build')
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader,
+                
+                'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.png|svg|jpg|jpeg$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'img/[name][ext]'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'style.css'
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'src/assets/img', to: 'img' },
+                { from: 'src/index.html' },
+            ]
+        }),
+    ],
+}
