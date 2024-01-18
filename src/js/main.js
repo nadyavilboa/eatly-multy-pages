21a6e31016dcf43f96eba5fa225b131293b65ea1
nadya <nadya.vilboa@list.ru>
2024-01-18 22:26:24 +0300

    Add tablet style

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,15 @@
-console.log('Hello world');
+const header = document.querySelector('.page-header');
+const menuBtn = header.querySelector('.burger');
+const menuBtnClose = header.querySelector('.menu__btn-close');
+const menu = header.querySelector('.menu');
+
+const menuOpen = () => {
+    menu.classList.add('menu--open');
+};
+
+const menuClose = () => {
+    menu.classList.remove('menu--open');
+};
+
+menuBtn.addEventListener('click', () => menuOpen());
+menuBtnClose.addEventListener('click', () => menuClose());
